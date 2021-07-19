6730e781729649b06ed565661155e2e410efc962
Build Service <svcscm@fb.com>
2021-07-19 02:55:07 -0700

    Adding quick start script

---
+++ quick_start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# work dir
+work_dir=$(realpath .)
+
+
+# npm version
+version=$(node --version)
+rgx='v([0-9]+)'
+[[ $version =~ $rgx ]]
+version="${BASH_REMATCH[1]}"
+if [ "$version" -lt 16 ]; then
+  echo "Error: Minimum version required for nodejs is: >=16.x"
+  exit 1
+fi
+
+# install deps
+cd $work_dir/client
+npm install
+npm run build
+cd $work_dir/server
+npm install
+npm start
+
